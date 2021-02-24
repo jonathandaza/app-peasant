806f1e888e6fb1b4a0ceb7730eb169d88fd58526
Jonathan <jdazav.5@gmail.com>
2021-02-24 22:56:39 +1300

    ADDED. bootstrap package was installed, as well as, component nav-bar was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
