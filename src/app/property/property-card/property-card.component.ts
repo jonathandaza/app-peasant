fadd5727aaaa9988a6aaa29709955bb928a6c6d3
Jonathan <jdazav.5@gmail.com>
2021-03-03 21:23:15 +1300

    ADDED. a property binging was created to pass the information through child component

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,13 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Products : any = {
-    "Id": 1,
-    "Name" : "Milk",
-    "Brand" : "Alquería",
-    "Type" : "Dairy",
-    "Price" : 18000
-  }
+  @Input() product: any;
 
   constructor() { }
 
